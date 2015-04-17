3b47fcc684a8884441eebefd79f25d7a2025c8cf
Ambika <ambikapsr@gmail.com>
2015-04-17 15:25:50 +0100

    PatientDemo

---
+++ PatientDemo.ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using PatientDemo.Domain.Entities;
+using System;
+
+namespace PatientDemo.ConsoleApp
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			Patient myPatient = new Patient();
+			myPatient.Surname = "Smith";
+			myPatient.Forename = "John";
+			Console.WriteLine("Name: " + myPatient.Forename + " " + myPatient.Surname);
+			Console.ReadLine();
+			Console.WriteLine("Hello {0} {1} Please enter your Date of Birth in YYYY/MM/DD format:", myPatient.Forename, myPatient.Surname);
+			string DOB = Console.ReadLine();
+			myPatient.DateOfBirth = Convert.ToDateTime(DOB);
+			Console.WriteLine("your Date of Birth is: " + myPatient.DateOfBirth);
+			Console.ReadKey();
+			Console.WriteLine("Enter your Gender:");
+			myPatient.Gender = Console.ReadLine();
+		}
+
+	}
+}
---
+++ PatientDemo.Domain/Entities/Patient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PatientDemo.Domain.Entities
+{
+    public class Patient
+    {
+        public string Surname { get; set; }
+        public string Forename { get; set; }
+		public DateTime DateOfBirth { get; set; }
+		public string Gender { get; set; }
+		public string PatientType { get; set; }
+    }
+}
